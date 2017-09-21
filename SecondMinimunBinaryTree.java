c8f9ac14deab8fcc6632742d3046d06e7ffcbc14
sirigowda <siri.m.gowda@gmail.com>
2017-09-21 02:41:54 -0700

    Adding new problem

---
+++ SecondMinimunBinaryTree.java
@@ -0,0 +1,40 @@
+
+public class SecondMinimunBinaryTree {
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+
+	public int findSecondMinimumValue(TreeNode root) {
+		if (root == null) {
+			return -1;
+		}
+		if (root.left == null && root.right == null) {
+			return -1;
+		}
+
+		int left = root.left.val;
+		int right = root.right.val;
+
+		// if value same as root value, need to find the next candidate
+		if (root.left.val == root.val) {
+			left = findSecondMinimumValue(root.left);
+		}
+		if (root.right.val == root.val) {
+			right = findSecondMinimumValue(root.right);
+		}
+
+		if (left != -1 && right != -1) {
+			return Math.min(left, right);
+		} else if (left != -1) {
+			return left;
+		} else {
+			return right;
+		}
+	}
+}
